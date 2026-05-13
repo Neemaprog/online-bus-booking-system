b870d9ccd756afca5a5b65cc74b6bfed3acf9807
Neemaprog <neematimotheo32@gmail.com>
2026-05-13 23:41:17 +0300

    Kuruhusu domain ya pythonanywhere kwenye allowed hosts

--- busproject/settings.py
+++ busproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^nq^ds5*lc+j=n2aec!5wv%jvk&j6zpxds5c4m6z=yq*0lki*0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['x']
+ALLOWED_HOSTS = ['neemaprog.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
